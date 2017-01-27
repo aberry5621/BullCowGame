0fa1f726ecc42a8e8470d91cf5a07070ad39ca5f
aberry5621 <aberry5621@gmail.com>
2017-01-27 09:52:14 -0800

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by ax on 1/27/17.
+//  Copyright © 2017 COMP235. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
